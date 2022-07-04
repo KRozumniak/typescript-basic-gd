42bfcfcd121062597bd81ed2daf05a1f25bee447
Kyrylo Rozumniak <kirkterra@gmail.com>
2022-07-03 22:00:57 -0400

    wrapping the module

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hey');
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Kirk';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
