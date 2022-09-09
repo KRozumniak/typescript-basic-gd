db119be445926cc0e508f77313b4372edd95af8e
Kyrylo Rozumniak <kirkterra@gmail.com>
2022-09-09 19:16:48 -0400

    Added project item

--- drag-n-drop/src/app.ts
+++ drag-n-drop/src/app.ts
@@ -122,7 +122,7 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   templateElement: HTMLTemplateElement;
   hostElement: T;
-  element: U;
+  element: U;  
 
   constructor(
     templateId: string,
@@ -154,8 +154,31 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     );
   }
 
-  abstract configure?(): void;
+  abstract configure(): void;
   abstract renderContent(): void;
+}
+
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+  renderContent() {
+    console.dir(this.element.querySelector('h2'));
+    
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent =
+      this.project.people.toString();
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
 }
 
 // ProjectList Class
@@ -196,9 +219,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     ) as HTMLUListElement;
     listEl.innerHTML = "";
     for (const projItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = projItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector("ul")!.id, projItem);
     }
   }
 }
