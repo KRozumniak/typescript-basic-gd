848fab66e87ccaf213867e4c159c4f05d45ad5b9
Kyrylo Rozumniak <kirkterra@gmail.com>
2022-09-19 19:48:58 -0400

    Added class transformer

--- libs/src/app.ts
+++ libs/src/app.ts
@@ -1,3 +1,19 @@
-import _ from 'lodash';
+import "reflect-metadata";
+import { plainToInstance } from 'class-transformer';
+import { Product } from "./product.model";
 
-console.log(_.shuffle([1, 2, 3]));
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+// const p1 = new Product('Book', 12.99);
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
