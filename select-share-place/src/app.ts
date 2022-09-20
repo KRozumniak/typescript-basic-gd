f11a655f4b05edf588c8b4696bf7708bf9649462
Kyrylo Rozumniak <kirkterra@gmail.com>
2022-09-19 21:18:59 -0400

    Added select-share-place

---
+++ select-share-place/src/app.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const form = document.querySelector("form") as HTMLFormElement;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+const _GOOGLE_API_KEY = "AIzaSyBV_nVspXvBVVSTMAQQFv68U2N1-rmct0o";
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO-RESULTS';
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAdress = addressInput.value;
+  console.log("yo");
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAdress
+      )}&key=${_GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== 'OK') {
+        throw new Error('Something went wrong!');
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 10,
+      });
+
+      new google.maps.Marker({position: coordinates, map: map});
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form?.addEventListener("submit", searchAddressHandler);
