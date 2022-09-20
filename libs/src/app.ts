5f7a51dbe9920691d8ddf2bcfc4af245e9ba90ab
Kyrylo Rozumniak <kirkterra@gmail.com>
2022-09-19 20:01:29 -0400

    Added class-validator

--- libs/src/app.ts
+++ libs/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { plainToInstance } from 'class-transformer';
 import { Product } from "./product.model";
+import { validate } from "class-validator";
 
 const products = [
   { title: "A Carpet", price: 29.99 },
@@ -11,6 +12,17 @@ const products = [
 // const loadedProducts = products.map((prod) => {
 //   return new Product(prod.title, prod.price);
 // });
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+  if (errors.length > 0) {
+    console.log('VALIDATION ERRORS!');
+    console.log(errors);
+  } else {
+    console.log(newProd);
+  }
+});
+
 
 const loadedProducts = plainToInstance(Product, products);
 
